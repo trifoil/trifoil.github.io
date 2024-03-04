f231de1a283c1074758ec8b5be8df24e8e3ec6a1
I͍͍̪̖̥͎̺͈̓ͯͨ̄̌̐̍̍ a͊ͦ̇ͤ̾m̴̴̸̵̧̡̛̞͔̮̖͉̝̥͔̟̺̼̟̈́̄̔ͦ̅͐ͦ̑͗ͬͮ́ͯ̓͘ a̓ d̪̜͗̀ͩ̐̽ͫ̃ͮ̽̃͢͞ǒ̘̤͐̇̃͒͝ͅr̋į̸͓̠ͤ́̋̓͊̆͋̄ͧ̒͆̚̕t̡͔͐̏ͅơ̩͍͍̯̮̪̰̯̟̻̙̯̋̊͌͑ͯͤͣ̓̎͗͒́͛̑̚͢ <143878567+trifoil@users.noreply.github.com>
2024-03-04 02:37:45 +0100

    Add files via upload

--- exampleforphp/index.php
+++ exampleforphp/index.php
@@ -4,18 +4,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Hello World</title>
+    <style>
+        body {
+            background-image: url('logo.png');
+            background-size: cover;
+            background-position: center;
+        }
+        .container {
+            text-align: center;
+            padding: 50px;
+        }
+        form {
+            display: inline-block;
+            background: rgba(255, 255, 255, 0.8);
+            padding: 20px;
+            border-radius: 10px;
+        }
+    </style>
 </head>
 <body>
-    <h1>PHP Hello World</h1>
-    
-    <?php
-    if(isset($_POST['submit'])) {
-        echo "<p>Hello</p>";
-    }
-    ?>
+    <div class="container">
+        <h1>PHP Hello World</h1>
+        
+        <?php
+        if(isset($_POST['submit'])) {
+            echo "<p>Hello</p>";
+        }
+        ?>
 
-    <form method="post">
-        <input type="submit" name="submit" value="Click Me">
-    </form>
+        <form method="post">
+            <input type="submit" name="submit" value="Click Me">
+        </form>
+    </div>
 </body>
 </html>
+
